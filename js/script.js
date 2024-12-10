b1357c893465e13d93919f62e83cc1d6d344e614
Victor Leonam <169005086+VicLeonam1@users.noreply.github.com>
2024-12-10 16:18:52 -0300

    asda

--- js/script.js
+++ js/script.js
@@ -22,19 +22,34 @@ const myObserver = new IntersectionObserver( (entries) => {
 
 elements.forEach( (element) => myObserver.observe(element) )
 
+
 const linha = document.querySelector('.linha')
 let lista = document.querySelector('.lista')
+const btn = document.querySelector('.exit')
 
 function menuHeader(){
-    if(lista.style.display === 'none'){
-        lista.style.display = 'none'
-    }else{
-        lista.style.display = 'block'
-    }
-
     if(lista.style.display === 'block'){
         lista.style.display = 'none'
+        btn.classList.remove('ativar')
     }else{
         lista.style.display = 'block'
+        btn.classList.add('ativar')
+    }
+}
+
+const secao = document.querySelector('.secao')
+
+function fecharSecao(){
+    if(window.innerWidth <= 768){
+        lista.style.display = 'none'
+        btn.classList.remove('ativar')
+    }
+}
+
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
     }
 }
